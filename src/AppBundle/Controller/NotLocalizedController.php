7d47d51056e280cec8dbe87b01f11fb62453a67c
oriodesign <oriodesign@gmail.com>
2014-12-30 17:36:26 +0100

    [REDIRECT] change to temporary redirect.

--- src/AppBundle/Controller/NotLocalizedController.php
+++ src/AppBundle/Controller/NotLocalizedController.php
@@ -14,6 +14,6 @@ class NotLocalizedController extends Controller
      */
     public function homepageNotLocalizedAction(Request $request)
     {
-        return $this->redirect($this->generateUrl('homepage', array('_locale' => $request->getPreferredLanguage(array_keys($this->container->getParameter('interface_translation_locales'))))), 301);
+        return $this->redirect($this->generateUrl('homepage', array('_locale' => $request->getPreferredLanguage(array_keys($this->container->getParameter('interface_translation_locales'))))), 302);
     }
 }
