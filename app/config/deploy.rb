088c5e914ceec4a84f6c417d8df1a2a4569114dc
Ruben Barilani <ruben.barilani.dev@gmail.com>
2015-04-15 01:06:44 +0200

    adds cap spike

---
+++ app/config/deploy.rb
@@ -0,0 +1,34 @@
+set :application, "landing-capifony"
+set :domain,      "apperclass.com"
+set :deploy_to,   "/home/ubuntu/sites/landing-capifony"
+set :app_path,    "app"
+
+
+set :user, "ubuntu"
+
+set :scm,         :git
+set :repository,  "https://github.com/apperclass/landing.git"
+set :branch, "topic/capifony"
+
+
+set :model_manager, "doctrine"
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain, :primary => true       # This may be the same as your `Web` server
+
+set  :use_sudo,      false
+set  :keep_releases,  3
+
+# Be more verbose by uncommenting the following line
+logger.level = Logger::MAX_LEVEL
+
+
+task :upload_parameters do
+  origin_file = "app/config/parameters_prod.yml"
+  destination_file = latest_release + "/app/config/parameters.yml" # Notice the latest_release.
+
+  try_sudo "mkdir -p #{File.dirname(destination_file)}"
+  top.upload(origin_file, destination_file)
+end
+
+before "deploy:share_childs", "upload_parameters"
