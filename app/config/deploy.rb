a5c2872b61f0d0b5f1dbfc77f8051bff82eaf1e9
Ruben Barilani <ruben.barilani.dev@gmail.com>
2015-04-15 10:26:44 +0200

    wip

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -16,13 +16,18 @@ set :model_manager, "doctrine"
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain, :primary => true       # This may be the same as your `Web` server
 
-set  :use_sudo,      false
+set  :use_sudo, false
 set  :keep_releases,  3
 
 # Be more verbose by uncommenting the following line
 logger.level = Logger::MAX_LEVEL
 
+#set :writable_dirs, ["app/cache", "app/logs"]
+#set :webserver_user, "www-data"
+#set :permission_method,  :chown
+#set :use_set_permissions, true
 
+# Load app/config/paramters_prod.yml TODO parametric
 task :upload_parameters do
   origin_file = "app/config/parameters_prod.yml"
   destination_file = latest_release + "/app/config/parameters.yml" # Notice the latest_release.
@@ -31,4 +36,5 @@ task :upload_parameters do
   top.upload(origin_file, destination_file)
 end
 
+# Events hooks
 before "deploy:share_childs", "upload_parameters"
